a7cc05e7fb9735599d1d447e96814031fbc67104
Robert Olive <robert.aaron.olive@gmail.com>
2018-06-22 17:20:08 -0400

    heroku setup

--- models/index.js
+++ models/index.js
@@ -1,4 +1,4 @@
 module.exports = {
-    Article: require("./Article"),
-    Note: require("./Note")
+    article: require("./article"),
+    comment: require("./comment")
   };
--- server.js
+++ server.js
@@ -1,21 +1,21 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-var logger = require("morgan");
-var mongoose = require("mongoose");
+const express = require("express");
+const bodyParser = require("body-parser");
+const logger = require("morgan");
+const mongoose = require("mongoose");
 
 // Our scraping tools
 // Axios is a promised-based http library, similar to jQuery's Ajax method
 // It works on the client and on the server
-var axios = require("axios");
-var cheerio = require("cheerio");
+const axios = require("axios");
+const cheerio = require("cheerio");
 
 // Require all models
-var db = require("./models");
+const db = require("./models");
 
-var PORT = 3000;
+const PORT = 3000;
 
 // Initialize Express
-var app = express();
+const app = express();
 
 // Configure middleware
 
@@ -27,4 +27,25 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/techScrape");
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
+
+// initialize handlebars
+const exphbs = require("express-handlebars")
+
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }))
+app.set("view engine", "handlebars")
+
+// API Routes
+app.get("/", function(req, res) {
+    res.render("index")
+})
+
+// Start the server
+
+app.listen(PORT, function() {
+    console.log("App running on port " + PORT + "!");
+});
